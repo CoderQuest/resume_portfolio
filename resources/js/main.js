325083607126d53991af86ca9315fcf0248b499f
Yi Ren <coderquestny@gmail.com>
2016-02-23 16:17:10 -0500

    Implemented skill bars.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-  
-  $('[data-toggle="tooltip"]').tooltip();  
+  // skill bar
+  $('.skillbar').each(function(){
+    $(this).find('.skillbar-bar').animate({
+      width:$(this).attr('data-percent')
+    }, 6000)
+  })
+
 });
