4da9a3d636064eace5e9c0c819548f07c6fae183
Yi Ren <coderquestny@gmail.com>
2016-02-23 20:40:16 -0500

    Created tabs for the web development experience section.

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
     }, 6000)
   })
 
+  // experience tabs
+  $('div.tabs h3').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('div.tabs h3').removeClass('current');
+    $('.tab-content').removeClass('current').slideUp(1000);
+
+    $(this).addClass('current');
+    $("#"+tab_id).addClass('current').slideDown(1000);
+  })
 });
